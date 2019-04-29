4fd73121665e39e0676d96742825624a268b0d7b
Igor T. Ghisi <igor.ghisi@gmail.com>
2019-04-28 23:24:31 -0300

    Fix not_implemented wrapper for Python3

--- src/python/qtodb/decorators.py
+++ src/python/qtodb/decorators.py
@@ -1,10 +1,11 @@
-__author__ = 'itghisi'
+from functools import wraps
 
 
 def not_implemented(foo):
 
+    @wraps(foo)
     def not_implemented_wrapper(self, *args, **kw):
-        function_fullname = "{0}.{1}".format(self.__class__.__name__, foo.func_name)
+        function_fullname = "{0}.{1}".format(self.__class__.__name__, foo.__name__)
         raise NotImplementedError("Method '{0}' not implemented".format(function_fullname))
 
     return not_implemented_wrapper
